64ea48e1e229648f44ed03b74badee8f75f3b93f
Nataly Rojas <natalyrojasm5@gmail.com>
2022-03-18 21:20:13 -0500

    ADD: Use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { CommentsComponent } from "./components/CommentsComponent";
 import { CommentsProvider } from "./context/CommentsContext";
 import "./scss/styles.scss";
 
 function App() {
   return (
     <CommentsProvider>
-      <h1>App</h1>
+      <CommentsComponent />
     </CommentsProvider>
   );
 }
--- src/components/ComponentsComponent.tsx
+++
@@ -1,5 +0,0 @@
-import { Comments } from "../interfaces/AppInterfaces";
-
-export const ComponentsComponent = ({}: Comments) => {
-  return <div>ComponentsComponent</div>;
-};
