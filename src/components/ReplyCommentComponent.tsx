79a6b4936f72ed5b4c39648490049caef295fb67
Nataly Rojas <natalyrojasm5@gmail.com>
2022-03-19 11:00:01 -0500

    UPDATE: Mobile layout completed

--- src/components/ReplyCommentComponent.tsx
+++ src/components/ReplyCommentComponent.tsx
@@ -1,7 +1,26 @@
-import { Reply } from "../interfaces/AppInterfaces";
+import { useContext } from "react";
+import { CommentsContext } from "../context/CommentsContext";
 
-const ReplyCommentComponent = ({}: Reply) => {
-  return <div>ReplyCommentComponent</div>;
+interface ReplyCommentProps {
+  btnText: string;
+  replyTo?: string;
+}
+
+const ReplyCommentComponent = ({ btnText }: ReplyCommentProps) => {
+  const {
+    user: {
+      username,
+      image: { webp },
+    },
+  } = useContext(CommentsContext);
+
+  return (
+    <div className="comment-box-reply shadow-sm bg-white px-3 py-4 mb-3">
+      <img src={webp} alt={username} />
+      <textarea className="px-3 py-2" name="reply" rows={3} />
+      <button className="px-3 py-2">{btnText}</button>
+    </div>
+  );
 };
 
 export default ReplyCommentComponent;
