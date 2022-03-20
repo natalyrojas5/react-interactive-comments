336cc9acf63cc9c75d7994e6a3540aa702195818
Nataly Rojas <natalyrojasm5@gmail.com>
2022-03-20 14:39:55 -0500

    ADD: Add comment

--- src/context/CommentsContext.tsx
+++ src/context/CommentsContext.tsx
@@ -1,10 +1,12 @@
 import React, { createContext, useReducer } from "react";
-import { AppState } from "../interfaces/AppInterfaces";
+import { AppState, Comment } from "../interfaces/AppInterfaces";
 import { CommentsReducers, state } from "./CommentsReducers";
 
 interface CommentsContextProps extends AppState {
-  addComment: () => void;
-  editComment: () => void;
+  updateActionComment: () => void;
+  updateStoreComment: () => void;
+  addComment: (comment: Comment) => void;
+  updateComment: () => void;
   deleteComment: () => void;
 }
 
@@ -17,21 +19,34 @@ interface ProviderProps {
 export const CommentsProvider = ({ children }: ProviderProps) => {
   const [appState, dispatch] = useReducer(CommentsReducers, state);
 
-  const addComment = () => {
-    dispatch({ type: "ADD_COMMENT" });
+  const addComment = (comment: Comment) => {
+    dispatch({ type: "ADD_COMMENT", payload: comment });
   };
 
-  const editComment = () => {
-    dispatch({ type: "EDIT_COMMENT" });
+  const updateComment = () => {
+    dispatch({ type: "UPDATE_COMMENT" });
+  };
+  const updateActionComment = () => {
+    dispatch({ type: "UPDATE_ACTION_COMMENT" });
   };
 
   const deleteComment = () => {
     dispatch({ type: "DELETE_COMMENT" });
   };
+  const updateStoreComment = () => {
+    dispatch({ type: "UPDATE_STORE_COMMENT" });
+  };
 
   return (
     <CommentsContext.Provider
-      value={{ ...appState, addComment, editComment, deleteComment }}
+      value={{
+        ...appState,
+        addComment,
+        updateActionComment,
+        deleteComment,
+        updateComment,
+        updateStoreComment,
+      }}
     >
       {children}
     </CommentsContext.Provider>
--- src/context/CommentsReducers.tsx
+++ src/context/CommentsReducers.tsx
@@ -1,14 +1,21 @@
-import { AppState } from "../interfaces/AppInterfaces";
+import { AppState, Comment } from "../interfaces/AppInterfaces";
 import { user, comments } from "../data/data";
 
 type CommentAction =
-  | { type: "ADD_COMMENT" }
-  | { type: "EDIT_COMMENT" }
+  | { type: "UPDATE_ACTION_COMMENT" }
+  | { type: "UPDATE_STORE_COMMENT" }
+  | { type: "ADD_COMMENT"; payload: Comment }
+  | { type: "UPDATE_COMMENT" }
+  | { type: "REPLY_COMMENT" }
   | { type: "DELETE_COMMENT" };
 
 export const state: AppState = {
   user,
   comments,
+  action: {
+    commentId: null,
+    mood: null,
+  },
 };
 
 export const CommentsReducers = (
@@ -17,8 +24,14 @@ export const CommentsReducers = (
 ): AppState => {
   switch (action.type) {
     case "ADD_COMMENT":
+      return { ...state, comments: [...state.comments, action.payload] };
+    case "UPDATE_COMMENT":
       return { ...state };
-    case "EDIT_COMMENT":
+    case "UPDATE_STORE_COMMENT":
+      return { ...state };
+    case "UPDATE_ACTION_COMMENT":
+      return { ...state };
+    case "REPLY_COMMENT":
       return { ...state };
     case "DELETE_COMMENT":
       return { ...state };
