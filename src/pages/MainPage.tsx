48905e0283b5f954f6d513c42c5ffd0980ff957f
Nataly Rojas <natalyrojasm5@gmail.com>
2022-03-20 17:29:44 -0500

    ADD: Modal delete in page y styles in file .scss

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,17 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
+
 import { CommentsComponent } from "../components/CommentsComponent";
+import ModalDelete from "../components/ModalDelete";
 import { CommentsContext } from "../context/CommentsContext";
 
 const MainPage = () => {
   const { comments } = useContext(CommentsContext);
+
   return (
     <>
       {comments.length === 0 && <p>Not comments</p>}
       {comments.length > 0 && <CommentsComponent />}
+      <ModalDelete />
     </>
   );
 };
