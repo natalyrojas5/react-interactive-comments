f6741435fcf50ad0aba1515d088e3588ce33ebca
Nataly Rojas <natalyrojasm5@gmail.com>
2022-03-21 21:54:18 -0500

    ADD: My profile in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,30 @@ import MainPage from "./pages/MainPage";
 import "./scss/styles.scss";
 
 function App() {
+  const goFrontendMentor = () => {
+    window.open("https://www.frontendmentor.io", "_blank");
+  };
+
+  const goMyProfile = () => {
+    window.open("https://www.frontendmentor.io/profile/natalyrojas5", "_blank");
+  };
   return (
     <CommentsProvider>
       <main>
         <MainPage />
       </main>
+      <footer className="mb-3">
+        <p>
+          Challenge by{" "}
+          <span role="button" onClick={goFrontendMentor}>
+            Frontend Mentor.
+          </span>{" "}
+          Coded by{" "}
+          <span role="button" onClick={goMyProfile}>
+            Nataly Rojas.
+          </span>
+        </p>
+      </footer>
     </CommentsProvider>
   );
 }
